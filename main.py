9f5dd3a9707c6362a599ca0a37df7324feed1dc3
Stéphane MENANTEAU <stephane.a.menanteau@socgen.com>
2019-06-20 10:26:34 +0200

    Some code on project

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 from client import News
+from enum import Enum
+
+class Code(Enum):
+    BOLD = '\033[1m'
+    DEF = '\033[0m'
 
 def main():
     news = News()
-    country = input("What country are you interested in?\n> ")
-    headlines = news.country_headlines(country)
+    choix = input('Country headlines [' + str(Code.BOLD) + 'default' + str(Code.DEF) +\
+         '] or Search [hit ' + str(Code.BOLD) + 's' + str(Code.DEF) + ']?\n> ')
+    if str(choix).lower() == 's':
+        search = input(f'What are you looking {Code.BOLD}for{Code.DEF}?')
+    else:
+        country = input("What country are you interested in?\n> ")
+        headlines = news.country_headlines(country)
     for headline in headlines:
         print(f"- {headline}")
 
